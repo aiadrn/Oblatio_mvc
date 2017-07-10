5cf81d819645782131a01f9cd3ce059ae6b06c21
aiadrn <adrian881015@gmail.com>
2017-07-10 10:50:32 -0500

    Modificación de la vista infoView  y  nuevas lineas en cssPrincipal

--- view/informacion/infoView.php
+++ view/informacion/infoView.php
@@ -2,22 +2,21 @@
     <h3 class="textoCentrado FuenteTitulo tamanioFuente2">INFORMACIÓN GENERAL</h3>
     <hr/>
 </div>
-<div id="" class="col-xs-12">
-    
-    <div class="col-md-4 col-md-offset-4">
-        <a href = "#" class="enlacesA">            
+<div id="" class="col-xs-12">    
+    <div class="col-md-6 col-md-offset-3">
+        <a href = "#" class="enlacesA ajusteA ajusteSuperiorA">            
             Descripción de la plataforma
         </a>
-        <a href = "#" class="enlacesA">            
+        <a href = "#" class="enlacesA ajusteA">            
             Políticas y condiciones
         </a>
-        <a href = "#" class="enlacesA">            
+        <a href = "#" class="enlacesA ajusteA">            
             ¿Cómo participar?
         </a>
-        <a href = "#" class="enlacesA">            
+        <a href = "#" class="enlacesA ajusteA">            
             Mécanica de juego
         </a>
-        <a href = "<?php echo "index.php?controller=sorteo&action=index"; ?>" class="btn btn-info btn-block">
+        <a href = "<?php echo "index.php?controller=sorteo&action=index"; ?>" class="btn btn-info btn-block ajusteBoton">
             <i class="glyphicon glyphicon-arrow-left"></i>
             Regresar
         </a> 
